1982d2c8edf800f5945bdf4ade6bf84a45d40668
Jaden Brown <jadenmb01@gmail.com>
2021-04-09 16:44:54 -0400

    initial commit

---
+++ click.js
@@ -0,0 +1,13 @@
+const buttons = document.querySelectorAll("buttons")
+const body = document.querySelector("body")
+
+for(let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(event) {
+        console.log(event.target.id);
+ })
+}
+const main = document.querySelector("main")
+const main = document.createElement("h3")
+const main = document.createElement("h2")
+const main = document.appendChild("h3")
+const main = document.appendChild("h2")
